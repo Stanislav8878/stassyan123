b9a8821949c6e622245f158fbbd46c11fc840d70
Станислав Степаненко <ronin999@list.ru>
2025-06-15 11:44:29 +0300

    main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pathlib import Path
 from file_handlers import read_csv_file, read_excel_file
 
 # Чтение CSV
-csv_transactions = read_csv_file(Path("transactions.csv"))
+csv_transactions = read_csv_file(Path("./data/transactions.csv"))
 print("CSV Transactions:", csv_transactions)
 
-# Чтение Excel
-excel_transactions = read_excel_file(Path("transactions_excel.xlsx"))
+# # Чтение Excel
+excel_transactions = read_excel_file(Path("./data/transactions_excel.xlsx"))
 print("Excel Transactions:", excel_transactions)
